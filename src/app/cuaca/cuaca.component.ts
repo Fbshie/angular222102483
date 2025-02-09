9bcfff8723a02505e29d7bbdf01d765ca1ab9129
Fbshie <rivatyas94@gmail.com>
2025-02-09 22:05:57 +0700

    set 9

--- src/app/cuaca/cuaca.component.ts
+++ src/app/cuaca/cuaca.component.ts
@@ -4,12 +4,14 @@ import { HeaderComponent } from "../header/header.component";
 import { SidebarComponent } from "../sidebar/sidebar.component";
 import { FooterComponent } from "../footer/footer.component";
 import { RouterModule } from '@angular/router';
+import { data } from 'jquery';
 
 declare const $: any;
 declare const moment: any;
 
 @Component({
   selector: 'app-cuaca',
+  standalone: true,
   imports: [RouterModule, HeaderComponent, SidebarComponent, FooterComponent],
   templateUrl: './cuaca.component.html',
   styleUrl: './cuaca.component.css'
@@ -17,44 +19,38 @@ declare const moment: any;
 export class CuacaComponent implements OnInit, AfterViewInit {
   private table1: any;
 
-  constructor(private renderer: Renderer2, private http: HttpClient) { }
+  constructor(private renderer: Renderer2, private http: HttpClient) {
+    this.renderer.removeClass(document.body, "sidebar-open");
+    this.renderer.addClass(document.body, "sidebar-closed");
+  }
 
   ngAfterViewInit(): void {
-    this.renderer.removeClass(document.body, "sidebar-open");
-    this.renderer.addClass(document.body, "sidebar-closed")
-
-    this.table1 = $("#table1").DataTable
-      (
-        {
-          "columnDefs":
-            [
+    this.table1 = $("#table1").DataTable({
+          "columnDefs": [
               {
                 "targets": 0,
-                "render": function (data: string) 
-                {
-                  var waktu = moment(data + " UTC");
+                "render": function (data: string) {
+                  const waktu = moment(data + " UTC");
                   console.log(waktu);
 
-                  var html = waktu.local().format("YYYY-MM-DD") + "<br />" + waktu.local().format("HH:mm") + "WIB";
+                  const html = waktu.local().format("YYYY-MM-DD") + "<br />" + waktu.local().format("HH:mm") + "WIB";
                   return html;
                 }
               },
               {
                 "targets": 1,
-                "render": function (data: string) 
-                {
+                "render": function (data: string) {
                   var html = "<img src='" + data + "' />";
                   return html;
                 }
               },
               {
                 "targets": 2,
-                "render": function (data: string) 
-                {
-                  var array = data.split('||');
-                  var cuaca = array[0];
-                  var deskripsi = array[1];
-                  var html = "<strong>" + cuaca + "</strong><br />" + deskripsi;
+                "render": function (data: string) {
+                  const array = data.split('||');
+                  const cuaca = array[0];
+                  const deskripsi = array[1];
+                  const html = "<strong>" + cuaca + "</strong><br />" + deskripsi;
                   return html;
                 }
               }
@@ -62,7 +58,49 @@ export class CuacaComponent implements OnInit, AfterViewInit {
         }
       );
 
-      this.bind_table1();
+    this.bind_table1();
+  }
+
+  getData(city: string): void {
+    city = encodeURIComponent(city);
+
+    this.http
+      .get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=ebb1ea45bb4a5290b5ec666e79d39c99`)
+      .subscribe((data: any) => {
+        let list = data.list;
+        console.log(list);
+
+        this.table1.clear();
+
+        list.forEach((element: any) => {
+          const weather = element.weather[0];
+          console.log(weather);
+
+          const iconUrl = "https://openweathermap.org/img/wn/" + weather.icon + "@2x.png";
+          const cuacaDeskripsi = weather.main + "|| " + weather.description;
+
+          const main = element.main;
+          console.log(main);
+
+          const tempMin = this.kelvinToCelcius(main.temp_min);
+          console.log({ tempMin });
+
+          const tempMax = this.kelvinToCelcius(main.temp_max);
+          console.log({ tempMax });
+
+          const temp = tempMin + "°C - " + tempMax + "°C";
+
+          const row = [element.dt_txt, iconUrl, cuacaDeskripsi, temp];
+
+          this.table1.row.add(row);
+        });
+
+        this.table1.draw(false);
+      }, (error: any) => {
+        alert(error.error.message);
+        this.table1.clear();
+        this.table1.draw(false);
+      });
   }
 
   bind_table1(): void {
@@ -91,7 +129,7 @@ export class CuacaComponent implements OnInit, AfterViewInit {
           var tempMax = this.kelvinToCelcius(main.temp_max);
           console.log("tempMax : " + tempMax);
 
-          var temp = tempMin + "o C -" + tempMax + "o C";
+          var temp = tempMin + "°C -" + tempMax + "°C";
 
           var row = [
             element.dt_txt,
@@ -108,12 +146,23 @@ export class CuacaComponent implements OnInit, AfterViewInit {
       );
   }
 
-  
+
 
   kelvinToCelcius(kelvin: any): any {
     var celcius = kelvin - 273.15;
     celcius = Math.round(celcius * 100) / 100;
     return celcius;
+  }
+
+  handleEnter(event: any) {
+    const cityName = event.target.value;
+
+    if (cityName == "") {
+      this.table1.clear();
+      this.table1.draw(false);
+    }
+
+    this.getData(cityName);
   }
 
   ngOnInit(): void {
